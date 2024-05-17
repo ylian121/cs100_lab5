316bcbdf487056f3dfca3fe2cd89bc5c799cdae6
“alandiazv” <adiaz205@ucr.edu>
2024-05-16 22:33:57 -0700

    added tests to test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,48 +1,47 @@
-#include "c-echo.h"
-#include "c-count.h"
+#include "rectangle.hpp"
 
 #include "gtest/gtest.h"
 
+TEST(ConstructorsTest, testDefault1) {
+    EXPECT_NO_THROW (Rectangle* testRectangle = new Rectangle());
+}
+
+TEST(ConstructorsTest, testParameter1) {
+    EXPECT_NO_THROW (Rectangle* testRectangle = new Rectangle(5, 10));
+}
+
+//needs to be created
 TEST(ConstructorsTest, testInput1) {
-    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
-    EXPECT_EQ("hello world", echo(3,test_val));
+
 }
 
-TEST(ConstructorsTest, testInput2) {
-    char* test_val[1]; test_val[0] = "./c-echo-count";
-    EXPECT_EQ("", echo(1, test_val));
+TEST(AreaTest, testInt1) {
+    Rectangle* testRectangle = new Rectangle(5, 10)
+    EXPECT_EQ(testRectangle->area(), 50)
 }
 
-TEST(ConstructorsTest, testInput3) {
-    char* test_val[1]; test_val[0] = "./c-echo-count";
-    EXPECT_EQ("", echo(1, test_val));
+TEST(AreaTest, testZero1) {
+    Rectangle* testRectangle = new Rectangle(0, 0)
+    EXPECT_EQ(testRectangle->area(), 0)
 }
 
-TEST(AreaTest, testInput1) {
-    std::string test_str = "hello world";
-    EXPECT_EQ(2, count(test_str));
-}
-
-TEST(AreaTest, testInput2) {
-    std::string test_str = "";
-    EXPECT_EQ(0, count(test_str));
-}
-
+//needs to be created
 TEST(AreaTest, testInput3) {
     std::string test_str = "";
     EXPECT_EQ(0, count(test_str));
 }
 
-TEST(PerimeterTest, testInput1) {
-    std::string test_str = "";
-    EXPECT_EQ(0, count(test_str));
+TEST(PerimeterTest, testInt1) {
+    Rectangle* testRectangle = new Rectangle(2, 4)
+    EXPECT_EQ(testRectangle->perimeter(), 12)
 }
 
-TEST(PerimeterTest, testInput2) {
-    std::string test_str = "";
-    EXPECT_EQ(0, count(test_str));
+TEST(PerimeterTest, testZero1) {
+    Rectangle* testRectangle = new Rectangle(0, 0)
+    EXPECT_EQ(testRectangle->perimeter(), 0)
 }
 
+//needs to be created
 TEST(PerimeterTest, testInput3) {
     std::string test_str = "";
     EXPECT_EQ(0, count(test_str));
